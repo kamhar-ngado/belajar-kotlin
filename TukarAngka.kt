dfcd3f6745e1f11487c8577b7f9a4b3ede160af3
kamhar-ngado <52423991+kamhar-ngado@users.noreply.github.com>
2020-01-10 10:35:24 +0700

    edit tukar angka

--- TukarAngka.kt
+++ TukarAngka.kt
@@ -1,9 +1,12 @@
-fun main(args: Array<String>)
-{
-    var angka1 = 10
-    var angka2 = 20
+fun main(args: Array<String>) {
+    var angka1:Int = 10
+    var angka2:Int = 20
 
-    println($angka1, $angka2)
+    var temp:Int = angka1
+    angka1 = angka2
+    angka2 = temp
 
-
+    println("pada mulanya (10, 20, ...n)")
+    println("saya akan menukar angka1 menjadi angka2 dan sebaliknya")
+    println("($angka1, $angka2, ...n)")
 }
