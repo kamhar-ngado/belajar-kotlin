6bac9a9d854292e5fec7e5b306bed32b773cc69e
kamhar-ngado <52423991+kamhar-ngado@users.noreply.github.com>
2020-01-10 11:22:56 +0700

    operasi matematika

---
+++ matematika.kt
@@ -0,0 +1,18 @@
+fun main(args: Array<String>)
+{
+    val a = 2
+    val b = 4
+
+    val sum = a+b
+    println("penjumlahan: $sum")
+
+    val sub = b-a
+    println("pengurangan: $sub")
+
+    val mul =a*b
+    println("perkalian: $mul")
+
+    val div = b/a
+    println("pembagian $div")
+
+}
