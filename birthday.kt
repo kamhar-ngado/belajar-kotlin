94d1a1c182dd3b02a5404c779564e1f01b847e9c
kamhar-ngado <52423991+kamhar-ngado@users.noreply.github.com>
2020-01-10 18:07:38 +0700

    menambahkan kalender

--- birthday.kt
+++ birthday.kt
@@ -1,9 +1,12 @@
+import java.util.*
+
 fun main(args: Array<String>)
 {
     print("tahun berapa kamu lahir: ")
     val birthday:Int = readLine()!!.toInt()
-    val umur:Int = 2020 - birthday
+    val kalender = Calendar.getInstance().get(Calendar.YEAR)
+    val umur:Int = kalender - birthday
 
-    print("umur kamu adalah $umur tahun")
+    print("umur kamu $umur tahun")
 
 }
