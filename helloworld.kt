2c28acdc6aacf3c1d24519dd36e13197de466e58
kamhar-ngado <52423991+kamhar-ngado@users.noreply.github.com>
2020-01-10 10:06:52 +0700

    edit helloworld

--- helloworld.kt
+++ helloworld.kt
@@ -3,10 +3,11 @@ fun main(args: Array<String>){
    val nama = "kamhar ngado"
    val prodi = "Pendidikan Matematika"
    val kelas = "PMATH C"
+   val angkatan = "2018"
 
-   println(nama)
-   println(prodi)
-   println(kelas)
+   println("nama saya adalah " + nama)
+   println("saya dari prodi " + prodi)
+   println("kelas saya $kelas " + "angkatan " + angkatan)
 
 
 
