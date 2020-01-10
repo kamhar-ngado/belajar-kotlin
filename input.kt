dcba49cf226e0ab20ae45ed5f3eda11cae9f2927
kamhar-ngado <52423991+kamhar-ngado@users.noreply.github.com>
2020-01-10 09:04:47 +0700

    readline String to String

--- input.kt
+++ input.kt
@@ -1,8 +1,13 @@
 fun main(args: Array<String>) 
 {
 
-    print("Enter text: ")
-    val stringInput:String = readLine()!!.toString
-    println("You entered: $stringInput")
+    val nama:String = readLine()!!.toString()
+    val IPK = 2.75
+    val umur = "24"
+
+    println("====info====")
+    println("Nama: $nama")
+    println("IPK: $IPK")
+    println("umur: $umur")
 
 }
