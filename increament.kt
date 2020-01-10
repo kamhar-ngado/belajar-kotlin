7ebf159af408738518ade8bb9830760da56f2424
kamhar-ngado <52423991+kamhar-ngado@users.noreply.github.com>
2020-01-10 16:53:17 +0700

    menambahkan increament

---
+++ increament.kt
@@ -0,0 +1,18 @@
+fun main()
+{
+    var x = 1
+    var y = 2
+    println("x = $x")
+    println("y = $y")
+
+    println("saya ingin menambahkan x+1")
+    x = x + 1
+    println("x = " + x)
+
+    println("kemudian saya juga mau menambahkan y = $y + 1")
+    y += 1
+    println("y = $y")
+
+
+
+}
