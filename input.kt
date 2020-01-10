f90098e173fa430bcabfe46de3885ef90de8b0f4
kamhar-ngado <52423991+kamhar-ngado@users.noreply.github.com>
2020-01-10 09:25:42 +0700

    user dapat menginput nilai pad sembarang variabel

--- input.kt
+++ input.kt
@@ -1,9 +1,12 @@
 fun main(args: Array<String>) 
 {
-
+    print("masukan nama:")
     val nama:String = readLine()!!.toString()
+    print("masukan IPK:")
     val IPK:Double = readLine()!!.toDouble()
+    print("masukan umur:")
     val umur = readLine()!!.toInt()
+    
 
     println("====info====")
     println("Nama: $nama")
