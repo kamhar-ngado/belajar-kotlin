7bf51961b6f78f5304e8704a67df7af59ab8c4bd
kamhar-ngado <52423991+kamhar-ngado@users.noreply.github.com>
2020-01-10 17:56:18 +0700

    program tebak umur

---
+++ birthday.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>)
+{
+    print("tahun berapa kamu lahir: ")
+    val birthday:Int = readLine()!!.toInt()
+    val umur:Int = 2020 - birthday
+
+    print("umur kamu adalah $umur tahun")
+
+}
