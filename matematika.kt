2ad5c05891ce3767dfea6d30fecf460ec63ecd56
kamhar-ngado <52423991+kamhar-ngado@users.noreply.github.com>
2020-01-10 11:41:13 +0700

    operasi matematika

--- matematika.kt
+++ matematika.kt
@@ -1,18 +1,21 @@
 fun main(args: Array<String>)
 {
-    val a = 2
-    val b = 4
+    print("masukan angka1:")
+    val angka1:Int = readLine()!!.toInt()
 
-    val sum = a+b
-    println("penjumlahan: $sum")
+    print("masukan angka2:")
+    val angka2:Int = readLine()!!.toInt()
 
-    val sub = b-a
-    println("pengurangan: $sub")
+    val sum = angka1+angka2
+    println("penjumlahan: $angka1 + $angka2 = $sum")
 
-    val mul =a*b
-    println("perkalian: $mul")
+    val sub = angka2-angka1
+    println("pengurangan: $angka2 - $angka1 = $sub")
 
-    val div = b/a
-    println("pembagian $div")
+    val mul =angka1*angka2
+    println("perkalian: $angka1 * $angka2 = $mul")
+
+    val div = angka2/angka1
+    println("pembagian: $angka2/$angka1 = $div")
 
 }
