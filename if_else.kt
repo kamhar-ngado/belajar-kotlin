e498fcf5883e876e01cc84ac911f82e7b29cdabc
kamhar-ngado <52423991+kamhar-ngado@users.noreply.github.com>
2020-01-11 14:57:53 +0700

    if_else

--- if_else.kt
+++ if_else.kt
@@ -1,17 +1,25 @@
 fun main(args: Array<String>)
+
 {
-    print("masukan mahar: ")
-    val mahar:Int = readLine()!!.toInt()
+    print("uang saya: ")
+    val harga_laptop:Int = readLine()!!.toInt()
+    if (harga_laptop >=26 && harga_laptop <= 29)
+    print("saya beli asus scar III hero")
+    else if (harga_laptop in 22..25)
+    {
+    println("saya akan beli Lenovo Y740")
+    }
+    else if (harga_laptop in 18..21)
+    {
+    println("saya akan beli laptop HP Pavilon")
+    }
+    else if (harga_laptop in 14..17)
+    {
+    println("saya akan beli laptop asus tuf")
+    }
+    else
+    {
+    println("saya tidak beli laptop")
+    }
 
-    if (mahar >= 15)
-    println("lamaranmu diterima")
-
-    if (mahar >= 9 && mahar <= 14 ) /*satuan juta */
-    println("lamaranmu ditolak")
-        else{
-                println("coba lagi")
-            }
-
-
-   
 }
