1c0507081346c1dddf4ead86a4697216abdd12b2
kamhar-ngado <52423991+kamhar-ngado@users.noreply.github.com>
2020-01-11 07:07:07 +0700

    menambahkan logika

--- TipeData.kt
+++ TipeData.kt
@@ -1,4 +1,5 @@
-fun main(args: Array<String>){
+fun main(args: Array<String>)
+{
 
 val nama = "kamhar ngado"
 val IPK = 2.75
---
+++ logika.kt
@@ -0,0 +1,38 @@
+/*dasar-dasar logika
+kurang dari (<)
+lebih dari (>)
+sama dengan (==)
+kurang dari sama dengan (<=)
+lebih dari sama dengan (>=)
+tidak sama dengan (!=)
+
+===logika and (&&)===
+True && True = True
+True && False = False
+False && True = False
+False && False = False
+
+===logika or (||) ====
+True || True = True
+True || False = True
+False || True = True
+False || False = False
+ */
+
+fun main(args: Array<String>)
+
+
+
+{
+/*logika sederhana */
+    val a:Boolean = 7>5
+    val b:Boolean = 5<8
+    val c:Boolean = 2==2
+    val d:Boolean = 2!=3
+
+    println(b)
+    println(a)
+    println(c)
+    println(d)
+
+}
