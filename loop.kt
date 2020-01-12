c0a8af975f3ffec15cbd18467f68c2aeccbd637a
kamhar-ngado <52423991+kamhar-ngado@users.noreply.github.com>
2020-01-12 14:56:32 +0700

    add step

--- loop.kt
+++ loop.kt
@@ -1,11 +1,11 @@
 fun main(args: Array<String>)
 
 {
-    for (i:Int in 1..5)
+    for (i:Int in 1..10 step 2 )
     {
         println("[$i]. kamhar handsome")
     }
-    for (j:Int in 6..10)
+    for (j:Int in 11..20 step 2 )
     {
         println("[$j]. kamhar cool")
     }
