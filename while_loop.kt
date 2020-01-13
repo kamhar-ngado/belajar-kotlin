d7340934497df5c7e74a1d0afa7ba33d7f6febbd
kamhar-ngado <52423991+kamhar-ngado@users.noreply.github.com>
2020-01-13 08:17:50 +0700

    edit

--- while_loop.kt
+++ while_loop.kt
@@ -1,9 +1,9 @@
 fun main(args: Array<String>)
 {
-   var i = 1
-    while (i <= 5) {
-        println("Line $i")
-        i += 1 //i = i + 1 or i++
+    var i = 1
+    while(i<11)
+    {
+        println("[$i]. saya adalah programer")
+        i++
     }
-
 }
