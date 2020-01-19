ae8644ae0772520b7580e5c0018aeb1635be2837
kamhar-ngado <52423991+kamhar-ngado@users.noreply.github.com>
2020-01-19 21:40:33 +0700

    add continue and break

--- ContinueBreak.kt
+++ ContinueBreak.kt
@@ -1,11 +1,24 @@
 fun main(args: Array<String>)
 {
+    println("contoh continue")
+    println("di bawah ini adalah bilangan bilangan prima")
     for (bilangan_prima:Int in 1..10)
     {
         if (bilangan_prima%2 == 0)
             {
                 continue
             }
-            print(bilangan_prima)
+            println(bilangan_prima)
+    }
+
+    println("contoh Break")
+    println("dibawah ini adalah bilangan bulat")
+    for (bilangan_bulat:Int in 11..20)
+    {
+        if (bilangan_bulat >=17 && bilangan_bulat < 20)
+        {
+            break
+        }
+        println(bilangan_bulat)
     }
 }
