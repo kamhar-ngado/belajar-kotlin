e5078cb0a3fd3d96d1e01ed45b21c03755b45a9c
kamhar-ngado <52423991+kamhar-ngado@users.noreply.github.com>
2020-01-19 11:07:41 +0700

    add continue dan Break

---
+++ ContinueBreak.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>)
+{
+    for (bilangan_prima:Int in 1..10)
+    {
+        if (bilangan_prima%2 == 0)
+            {
+                continue
+            }
+            print(bilangan_prima)
+    }
+}
